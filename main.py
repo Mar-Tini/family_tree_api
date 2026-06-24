e2319d1cd598da1111cf7e2e74f539b4c9cc914e
Mar-Tini <mart00tini@gmail.com>
2026-06-24 10:12:05 +0300

    feat ; update code

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://family-tree-n7zw3jxrs-0-tinis-projects.vercel.app",
-    "https://family-tree-n7zw3jxrs-0-tinis-projects.vercel.app",
     "https://family-tree-api-3pbs.onrender.com"
 ]
 
